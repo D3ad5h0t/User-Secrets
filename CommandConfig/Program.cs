d0dcddd1a872658e5d11708b120f2f311cdcf69b
Alexander Glushchenko <alexander.glushchenko@aspose.com>
2023-06-07 12:19:28 +1000

    init commit

---
+++ CommandConfig/Program.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.Extensions.Configuration;
+
+IConfigurationBuilder builder = new ConfigurationBuilder()
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+    .AddUserSecrets(typeof(Program).Assembly, optional: true);
+
+IConfigurationRoot config = builder.Build();
+
+Console.WriteLine("I think the password is ... " + config["Password"]);
